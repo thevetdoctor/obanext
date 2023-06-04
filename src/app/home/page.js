2058bed5344e831fb79a7c21e3024dc6c95045d1
obafemi oderanti <thevetdoctor@gmail.com>
2023-06-04 23:21:28 +0100

    update gradient on homepage

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -11,12 +11,17 @@ export default function App() {
       initialValues={{ file: null }}
       onSubmit={(values, { setSubmitting }) => {
         // Handle file upload here
+        event.preventDefault()
         console.log("values", values.file);
         setSubmitting(false);
       }}
     >
       {({ values, errors, handleSubmit, setFieldValue }) => (
-        <form onSubmit={handleSubmit} className="mt-10 pb-20 h-1/2 px-5 items-center flex">
+        <form 
+        onSubmit={handleSubmit} 
+        className="mt-10 pb-20 h-1/2 px-5 items-center flex"
+        encType="multipart/form-data"
+        >
           <Field
             type="file"
             name="file"
@@ -29,7 +34,10 @@ export default function App() {
           />
           {errors.file && <div>{errors.file}</div>}
           {values.file && <div>{values.file.name}</div>}
-          <button type="submit">Submit</button>
+          <button
+            className="hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            type="submit"
+          >Submit</button>
         </form>
       )}
     </Formik>
