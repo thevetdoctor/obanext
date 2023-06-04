9c2cc1df3aafed34f31bd9f3cfc096a4d3d3a9a3
obafemi oderanti <thevetdoctor@gmail.com>
2023-06-04 10:24:13 +0100

    implement home page

--- src/app/page.js
+++ src/app/page.js
@@ -25,6 +25,7 @@ export default function Home() {
         ]
           .map((fruit) => (
         <a
+          key={fruit.slug}
           href={fruit.link}
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
